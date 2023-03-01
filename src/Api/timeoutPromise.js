586bbb182de9c4cd7dfe3bb703d78c3fccac9cb7
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-03-01 09:11:30 +0100

    added suspense

---
+++ src/Api/timeoutPromise.js
@@ -0,0 +1,10 @@
+// import wrapPromise from "./wrapPromise";
+
+// function timeoutPromise() {
+//   return new Promise((res, rej) => {
+   
+//   });
+// }
+
+// const timeoutPromiseResource = wrapPromise(timeoutPromise);
+// export default timeoutPromiseResource;
